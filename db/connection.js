cb808624a0d3d4fbfc18b6f2864bf9950d7f6008
Vaugusto <samlara273@gmail.com>
2026-01-28 12:13:35 -0300

    ✨ Connected routes on server.js   :bug: Fixed minor issues (int to integer) and turned on foreign keys

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,6 @@
 import Database from "better-sqlite3";
 
 const db = new Database("./db/app.db");
+db.pragma("foreign_keys = ON")
 
 export default db;
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 import express from "express";
+import {initDb} from "./db/schema.js";
+
+import productsRoutes from "./routes/products.js"
+import categoriesRoutes from "./routes/categories.js"
 
 const app = express();
 const PORT = 3000;
+
 app.use(express.json());
 
-app.post("/products", (req, res) => {
-	products.push(req.body);
+initDb();
 
-    res.status(201).json(req.body);
-});
-
-app.get("/products", (req, res) => {
-	res.status(200).res.json(products);
-});
+app.use("/products", productsRoutes);
+app.use("/categories", categoriesRoutes);
 
 app.listen(PORT, () => {
     console.log(`O Servidor está rodando na porta ${PORT}`);
