56ec45011567839dfbeb06437ab5e3ec4733b760
Vaugusto <samlara273@gmail.com>
2026-01-29 11:57:55 -0300

    ✨Added functional Post and Get methods.

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,5 +1,19 @@
-export function list(req, res) { return res.json([]); }
+import db from "../db/connection.js";
+
+export function list(req, res) { 
+    const returnAll = db.prepare('SELECT * FROM products')
+    const products = returnAll.all();
+    return res.status(200).json(products); 
+}
 export function getById(req, res) { return res.json({}); }
-export function create(req, res) { return res.status(201).json({}); }
+export function create(req, res) { 
+    const { category_id, name, desc, image_url } = req.body;
+    const stmt = db.prepare(`INSERT INTO products (category_id, name, desc, image_url) VALUES (?, ?, ?, ?)`);
+    const create = stmt.run(category_id, name, desc, image_url);
+    return res.status(201).json({
+        id: create.lastInsertRowid,
+        message: 201
+    }); 
+}
 export function update(req, res) { return res.json({}); }
 export function remove(req, res) { return res.status(204).send(); }
